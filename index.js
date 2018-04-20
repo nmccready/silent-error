6e6b11e1c4b9a5633ee01e2d3eb9065ad5d48332
nmccready <nemtcan@gmail.com>
2018-04-20 12:47:35 -0400

    linting and prettier
    gitignore

--- index.js
+++ index.js
@@ -6,12 +6,15 @@ function SilentError(message) {
     throw new TypeError('SilentError must be instantiated with `new`');
   }
 
-  this.name          = 'SilentError';
-  this.message       = message;
+  this.name = 'SilentError';
+  this.message = message;
   this.isSilentError = true;
 
-  if (process.env.SILENT_ERROR === 'verbose' || process.env.EMBER_VERBOSE_ERRORS === 'true') {
-    this.stack = (new Error()).stack;
+  if (
+    process.env.SILENT_ERROR === 'verbose' ||
+    process.env.EMBER_VERBOSE_ERRORS === 'true'
+  ) {
+    this.stack = new Error().stack;
     this.suppressedStacktrace = false;
   } else {
     this.suppressedStacktrace = true;
@@ -23,7 +26,7 @@ SilentError.prototype.constructor = SilentError;
 
 SilentError.debugOrThrow = function debugOrThrow(label, e) {
   // if the error is a SilentError, ignore
-  if(e && e.isSilentError) {
+  if (e && e.isSilentError) {
     // ignore this error, invalid blueprints are handled in run
     debug(label)(e);
   } else {
